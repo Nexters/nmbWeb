2586939dbfcccbe7551e59ed3b9feeec15974cb1
wow5468 <wow5468@naver.com>
2016-08-02 14:06:47 +0900

    Initial commit

---
+++ src/main/java/com/teamnexters/nmbweb/NmbwebApplication.java
@@ -0,0 +1,12 @@
+package com.teamnexters.nmbweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmbwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmbwebApplication.class, args);
+	}
+}
